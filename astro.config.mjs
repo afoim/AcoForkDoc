463af7d47bef3ce830ec4c5188a5b31b0a4ecadb
afoim <acofork@qq.com>
2025-08-10 13:46:43 +0800

    feat: 初始化AcoFork文档项目
    添加基础项目结构，包括配置文件、文档内容、静态资源和依赖项

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'AcoFork Docs',
+			favicon: 'src/assets/acofork.jpg',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/afoim/AcoForkDoc' }],
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: 'guides/example' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
+		}),
+	],
+});
